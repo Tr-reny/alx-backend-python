cf553adac8d6eac80d416c0e946071ccecc396d4
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-04-28 00:59:40 -0400

    Update 102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -9,14 +9,3 @@ def zoom_array(lst: Tuple[int], factor: int = 2) -> List[int]:
         for i in range(factor)
     ]
     return zoomed_in
-
-
-if __name__ == "__main__":
-    array = (12, 72, 91)
-
-    zoom_2x = zoom_array(array)
-
-    zoom_3x = zoom_array(array, 3)
-
-    print(zoom_2x)
-    print(zoom_3x)
