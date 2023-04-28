64ff5f838a864cb910e9faf722fc6bcbe73022e1
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-04-28 00:31:34 -0400

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains a function 'add' that
+takes two floats as arguments and returns their sum.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats.
+
+    Args:
+        a: the first float
+        b: the second float
+
+    Returns:
+        The sum of a and b as a float.
+    """
+    return a + b
